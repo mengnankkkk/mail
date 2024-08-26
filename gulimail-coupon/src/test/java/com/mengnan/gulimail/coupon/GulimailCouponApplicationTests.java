248d1ef2ec60ed71a8bc697d0e240f71f754b045
mengnankkkk <mengnankkzhou@gmail.com>
2024-08-26 20:29:59 +0800

    git init

---
+++ gulimail-coupon/src/test/java/com/mengnan/gulimail/coupon/GulimailCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mengnan.gulimail.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ gulimail-member/src/test/java/com/mengnan/gulimail/member_/GulimailMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mengnan.gulimail.member_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailMemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
